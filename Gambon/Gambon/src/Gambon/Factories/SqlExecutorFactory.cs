814ac2ecd7e5cfb96caa865bfbd1135ad74b0459
Seymour Poler <seymourpoler@gmail.com>
2018-02-26 19:20:14 +0100

    adding new project

---
+++ Gambon/Gambon/src/Gambon/Factories/SqlExecutorFactory.cs
@@ -0,0 +1,14 @@
+﻿using Gambon.SqlServer;
+
+namespace Gambon.Factories
+{
+    public class SqlExecutorFactory
+    {
+        public static ISqlExecutorWithGeneric SqlExecutorWithGeneric(string connectionString)
+        {
+            return new SqlExecutorWithGeneric(
+                        sqlConnectionFactory: new SqlConnectionFactory(
+                            connectionString: connectionString));
+        }
+    }
+}
---
+++ Gambon/Gambon/src/Gambon/ISqlExecutorWithGeneric.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Gambon
+{
+    public interface ISqlExecutorWithGeneric
+    {
+        IEnumerable<T> ExecuteReader<T>(string sql) where T : class;
+        int ExecuteNonQuery(string sql);
+    }
+}
